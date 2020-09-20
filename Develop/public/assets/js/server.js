4c4309f303825322d164b6a0c91a034145799c65
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-09-20 11:58:36 -0500

    pushing files

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const apiRoutes = require("../routes/apiroute.js");
+const htmlRoutes = require("../routes/htmlroute.js");
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
